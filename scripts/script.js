fd4241ae77fcf0aa47eefc04c1bc7cccdb37a99c
InuwaSomachi <115094023+InuwaSomachi@users.noreply.github.com>
2025-03-08 15:37:54 -0800

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function enterSite() {
+    window.location.href = "chapters/chapter1.html";
+}
+
+// Sound effect function
+function playBooLaugh() {
+    const audio = new Audio('assets/audio/boo-laugh.mp3');
+    audio.play();
+}
